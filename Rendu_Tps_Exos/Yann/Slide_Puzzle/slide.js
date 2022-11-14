5edf23a681b6b787b9c5cb19d19ce8d1563fb93d
yannformation <cotardyann@orange.fr>
2022-11-14 11:16:22 +0100

    2ème partie jeu taquin

--- Rendu_Tps_Exos/Yann/Slide_Puzzle/slide.js
+++ Rendu_Tps_Exos/Yann/Slide_Puzzle/slide.js
@@ -1,6 +1,6 @@
 //Mélange aléatoire
 function melange(){
-var alea = document.querySelector('.jeu');
+var alea = document.querySelectorAll('#case');
 for (var i = alea.children.length; i>=0; i--){
     alea.appendChild(alea.children[Math.random() * i | 0]);
 }
@@ -11,9 +11,33 @@ for (var i = alea.children.length; i>=0; i--){
 //mémoriser l'emplacement de la case vide
 var emptyLign= 4;
 var emptyCol=4;
-
+var nbClick = 0;
 //fonction qui échange la case cliquée avec la case vide
 function move(lign, col){
+    if(
+        ((emptyLign==lign) && ((col==emptyCol-1) || (col==emptyCol+1)))
+    ||  ((emptyCol==col) && ((lign==emptyLign-1) || (lign==emptyLign+1)))
+    ){
+
+    //maj nombre clicks
+    nbClick=nbClick+1;
+    //récuprération du compteur
+    var compteurClick= document.getElementById('compteurClick');
+
+    //couper fil ducompteur
+    compteurClick.removeChild(compteurClick.childNodes[0]);
+
+    //amener la valeur clicks
+    var compteurText;
+    if(nbClick==1){
+        compteurText=document.createTextNode('Un seul coup joué !')
+    }else{
+        compteurText=document.createTextNode(nbClick+' coups joués !')
+    }
+
+    compteurClick.appendChild(compteurText);
+
+
     //récupération des noms des case cliquée et vide
     var clickName= 'case'+lign+col;
     var emptyName= 'case'+emptyLign+emptyCol;
@@ -40,4 +64,5 @@ function move(lign, col){
     //on mémorise le nouvel emplacement de la case vide
     emptyLign = lign,
     emptyCol = col;
+}
 }
