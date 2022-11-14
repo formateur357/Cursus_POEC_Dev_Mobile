7bc2dd8d9c2a942952a62c160a7afdbfb291732e
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-11-14 12:06:27 +0100

    supression code pour taquin à 9 case

--- Rendu_Tps_Exos/Tarik/exo_js/taquin/script.js
+++ Rendu_Tps_Exos/Tarik/exo_js/taquin/script.js
@@ -1,25 +1,18 @@
-let btn1 = document.getElementById("btnUn");
-let btn2 = document.getElementById("btnDeux");
-let btn3 = document.getElementById("btnTrois");
-let btn4 = document.getElementById("btnQuatre");
-let btn5 = document.getElementById("btnCinq");
-let btn6 = document.getElementById("btnSix");
-let btn7 = document.getElementById("btnSept");
-let btn8 = document.getElementById("btnHuit");
-let btn9 = document.getElementById("btnNeuf");
-let btn10 = document.getElementById("btnDix");
-let btn11 = document.getElementById("btnOnze");
-let btn12 = document.getElementById("btnDouze");
-let btn13 = document.getElementById("btnTreize");
-let btn14 = document.getElementById("btnQuatorze");
-let btn15 = document.getElementById("btnQuinze");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
 
 let empty = document.getElementById("empty");
 let nbrTour = document.getElementById("nbrTour");
 let count = 0;
+let gameFinish = document.getElementById("gameFinish");
 
-let arrayOfBtn = [ btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8,
-    btn9, btn10, btn11, btn12, btn13, btn14, btn15, empty];
+let arrayOfBtn = [ btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, empty];
 
 
 
@@ -32,7 +25,7 @@ function changePlace(btn) {
 
     if (
         orderBtn == orderEmpty - 1 || orderBtn == orderEmpty + 1 ||
-        orderBtn == orderEmpty - 4 ||orderBtn == orderEmpty + 4 )
+        orderBtn == orderEmpty - 3 ||orderBtn == orderEmpty + 3 )
     {
         document.getElementById(btn).style.order = orderEmpty;
         empty.style.order = orderBtn;
@@ -42,14 +35,27 @@ function changePlace(btn) {
     } else {
         console.log("Déplacement interdit");
     }
+    checkFinish()
 }
 
-function randomStart() {
-    arrayOfBtn.sort(() => Math.random() - 0.5);
-    console.log(arrayOfBtn)
-    for (let i = 0; i < arrayOfBtn.length ; i++) {
-        arrayOfBtn[i].style.order = i+1;
+// function randomStart() {
+//     arrayOfBtn.sort(() => Math.random() - 0.5);
+//     console.log(arrayOfBtn)
+//     for (let i = 0; i < arrayOfBtn.length ; i++) {
+//         arrayOfBtn[i].style.order = i+1;
+//     }
+// }
+// randomStart();
+
+function checkFinish() {
+    for (let i = 1; i < 9; i++) {
+        if (getComputedStyle(document.getElementById(`btn${i}`)).order == i) {
+            console.log(`btn${i} checked`);
+        } else {
+          return false;
+        }
     }
-}
+    console.log("fini!!!!!!");
+    gameFinish.innerHTML = "Partie terminée !!"
 
-randomStart();
+}
