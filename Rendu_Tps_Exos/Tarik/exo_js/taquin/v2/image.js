6ad2de2664cff20df862269589064328eb8649cd
KinguBaka <tarikab92@yahoo.fr>
2022-11-18 12:15:06 +0100

    création v3

--- Rendu_Tps_Exos/Tarik/exo_js/taquin/v2/image.js
+++
@@ -1,7 +0,0 @@
-export default listImg = {
-    "img1" : "./img/chat_taquin.png",
-    "img2" : "./img/chat2.jpg",
-    "img3" : "./img/chat3.jpg",
-    "img4" : "./img/chat4.jpg",
-    "img5" : "./img/raton.jpg"
-}
