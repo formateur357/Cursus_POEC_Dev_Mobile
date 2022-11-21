c82b618039eb2371eb486f57adec80a937130fc5
william9942 <williamvvv333@gmail.com>
2022-11-21 17:01:03 +0100

    La Galère

---
+++ Rendu_Tps_Exos/William/Exercices HTML to JAVASCRIPT/exoHtmltoJS.js
@@ -0,0 +1,24 @@
+
+/*Ecrire la fonction changer_style qui permet de styler le paragraphe au clic du bouton par :
+une couleur blanche.
+un background noir.
+une bordure noire pointillée de 1px.
+un retrait de 5px.
+Définir les propriétés précédentes dans une classe "active" et modifier la fonction changer_style de telle façon qu'elle ajoute la classe "active" au paragraphe.
+*/
+
+
+function change_style (color) 
+{
+    var whitecolor = document.getElementById('whitecolor');
+
+var cliquer = document.getElementById('cliquer);
+cliquer.addEventListener(’click’, function() {
+var nom = document.getElementById(’nom’).
+value;
+alert ("Bonjour " + nom);
+});
+
+/*var paragraphes = document.getElementsByTag("p");
+for (var elt of paragraphes)
+console.log(elt.innerHTML);*/
