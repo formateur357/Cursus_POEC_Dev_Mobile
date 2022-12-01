4fbf2204b84cb4917f289a02d6716f564a79e0bb
denisalarcon <romero270814@gmail.com>
2022-12-01 15:06:30 +0100

    angularexo3

--- Rendu_Tps_Exos/Denis/angular/exotodo/src/app/app.component.ts
+++ Rendu_Tps_Exos/Denis/angular/exotodo/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Task } from './class/task.model';
 import {  } from '@angular/core';
 import { zip } from 'rxjs';
+import { TodolistService } from './service/todolist.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,23 +11,23 @@ import { zip } from 'rxjs';
 export class AppComponent {
   public title: string;
   public count : number;
-  public tasks: Task[];
+  //public tasks: Task[];
  
   pourcentage: number= 0;
   public percent: number;
    
-  constructor(){
+  constructor(public tls : TodolistService){
     this.title = "todo";
     this.count = 1;
-    this.tasks = [
+   /* this.tasks = [
       new Task(0, "gatito", true, "ppppp"),
       new Task(1, "perrito", true, "cccc"),
       new Task(2, "puerquito", false, "ffff"),
       new Task(4, "ratoncito", true, "dddd")
-    ];
-    this.count = this.tasks.filter((task) => task.completed).length;
+    ];*/
+    this.count = this.tls.tasks.filter((task) => task.completed).length;
     this.percent =
-      this.tasks.length > 0 ? (this.count / this.tasks.length) * 100 : 0;
+      this.tls.tasks.length > 0 ? (this.count / this.tls.tasks.length) * 100 : 0;
   }
 
   trackByFunction(index: number, item: any): string {
--- Rendu_Tps_Exos/Denis/angular/exotodo/src/app/service/todolist.service.ts
+++ Rendu_Tps_Exos/Denis/angular/exotodo/src/app/service/todolist.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
+/*import { Injectable } from '@angular/core';
+import { Task } from '../class/task.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodolistService {
+  public tasks: Task[];
+  constructor() {
+    this.tasks = [
+      new Task(0, "gatito", true, "ppppp"),
+      new Task(1, "perrito", true, "cccc"),
+      new Task(2, "puerquito", false, "ffff"),
+      new Task(4, "ratoncito", true, "dddd")
+    ];
+    
+   }
+}*/
+import { Injectable } from '@angular/core';
+import { Task } from '../class/task.model';
 
-  constructor() { }
+const initialList = [
+  new Task(0, 'gatito', true, 'pppp'),
+  new Task(1, 'perrito', true, 'cccc'),
+  new Task(2, 'puerquito', false, 'fffff'),
+  new Task(3, 'ratoncito', true, 'ddddd'),
+];
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodolistService {
+  public tasks: Task[] = [];
+
+  constructor() {
+    this.updateList(initialList);
+  }
+
+  public updateList(list: Task[]) {
+    new Promise(() => {
+      setTimeout(() => {
+        this.tasks = list;
+      }, 1000);
+    });
+  }
+  public toggleComplete(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+  }
 }
+  
